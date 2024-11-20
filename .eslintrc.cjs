cc15b92f317424aebab6db739807d207afcb034e
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-11-20 14:26:12 -0500

    lint issue resolved

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,31 @@ module.exports = {
         es2020: true,
         node: true,
     },
-    extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@babel/eslint-parser', 
+    parserOptions: {
+        requireConfigFile: false, 
+        ecmaVersion: 2020,
+        sourceType: 'module', 
+        babelOptions: {
+            presets: ['@babel/preset-react'],
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
     plugins: ['prettier'],
     rules: {
         'comma-dangle': ['error', 'only-multiline'],
         'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        // indent: ["error", 4],
-        'no-unused-vars': ['warn'],
-        // "no-console": ["warn"],
+        'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], 
+        'react/react-in-jsx-scope': 'off',
     },
-    settings: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
