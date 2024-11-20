abe91ae2ce122cbff4b69aacfd37f4c749417ae3
GinaCastromonte <123599985+GinaCastromonte@users.noreply.github.com>
2024-11-20 14:10:11 -0500

    eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,33 +5,15 @@ module.exports = {
         es2020: true,
         node: true,
     },
-    parserOptions: {
-        sourceType: 'module',
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'prettier',
-        'plugin:prettier/recommended',
-    ],
+    extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
-    plugins: ['react-refresh', 'react-hooks', 'prettier'],
+    plugins: ['prettier'],
     rules: {
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
         'comma-dangle': ['error', 'only-multiline'],
-        'react/prop-types': 'off',
-        'react/display-name': 'off',
         'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        // indent: ['error', 4],
+        // indent: ["error", 4],
         'no-unused-vars': ['warn'],
-        // 'no-console': ['warn'],
+        // "no-console": ["warn"],
     },
-    settings: {
-        react: {
-            version: 'detect', 
-        },
-    },
+    settings: {},
 };
