9bf44848ed0e643de98d16cc7f0621d6d6e8b672
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-11-22 09:35:46 -0500

    set up pages and routes

---
+++ src/App2.jsx
@@ -0,0 +1,82 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import {
+  AddDrug,
+  AdminAddUser,
+  Alerts,
+  Dashboard,
+  DispenseDrug,
+  Landing,
+  Login,
+  Register,
+  Reports,
+  Store,
+  User,
+  Error,
+  EditDrug,
+  HomeLayout,
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ],
+  },
+
+  {
+    path: '/alerts',
+    element: <Alerts />,
+  },
+  {
+    path: '/adddrug',
+    element: <AddDrug />,
+  },
+  {
+    path: '/editdrug',
+    element: <EditDrug />,
+  },
+  {
+    path: '/dispense',
+    element: <DispenseDrug />,
+  },
+  {
+    path: '/store',
+    element: <Store />,
+  },
+  {
+    path: '/user',
+    element: <User />,
+  },
+  {
+    path: '/adminuser',
+    element: <AdminAddUser />,
+  },
+  {
+    path: '/reports',
+    element: <Reports />,
+  },
+])
+
+const App2 = () => {
+  return <RouterProvider router={router} />
+}
+export default App2
---
+++ src/pages/AddDrug.jsx
@@ -0,0 +1,9 @@
+const AddDrug = () => {
+  return (
+    <div>
+      <h1>Add Drug</h1>
+    </div>
+  )
+}
+
+export default AddDrug
