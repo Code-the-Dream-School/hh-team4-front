125e24738981080470c6aac1a1a5568e6f4d6ae8
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-11-25 15:17:50 -0500

    changed the path to App.jsx and index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App2 from './App2.jsx';
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
