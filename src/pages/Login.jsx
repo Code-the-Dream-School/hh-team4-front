1e8d1a95193379ddebb62d199542eeadf05cdba1
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-12-13 10:29:19 -0500

    save user info on front end and pull user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,21 +4,48 @@ import { FormRow, Logo } from '../components';
 import { toast } from 'react-toastify';
 import customFetch from '../util/customFetch';
 
+// export const action = async ({ request }) => {
+//     const formData = await request.formData();
+//     const data = Object.fromEntries(formData);
+//     const errors = { msg: '' };
+//     if (data.password.length < 3) {
+//         errors.msg = 'Password too short';
+//         return errors;
+//     }
+//     try {
+//         await customFetch.post('/auth/login', data);
+//         toast.success('Login Successful');
+//         return redirect('/dashboard');
+//     } catch (error) {
+//         toast.error(error?.response?.data?.msg);
+//     }
+//     return null;
+// };
+
 export const action = async ({ request }) => {
     const formData = await request.formData();
     const data = Object.fromEntries(formData);
+
     const errors = { msg: '' };
+
     if (data.password.length < 3) {
         errors.msg = 'Password too short';
         return errors;
     }
+
     try {
-        await customFetch.post('/auth/login', data);
+        const response = await customFetch.post('/auth/login', data);
+        const { token, user } = response.data;
+        //SET THEM IN LOCAL STORAGE
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', user.id);
         toast.success('Login Successful');
         return redirect('/dashboard');
     } catch (error) {
-        toast.error(error?.response?.data?.msg);
+        // Handle errors
+        toast.error(error?.response?.data?.msg || 'Something went wrong');
     }
+
     return null;
 };
 
