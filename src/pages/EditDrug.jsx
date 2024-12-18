b4b42e92e53baaaa54baf9fe8a8ca377e2b425f0
sarross88 <94658493+sarross88@users.noreply.github.com>
2024-12-18 07:39:06 -0500

    forms fixed with select options

--- src/pages/EditDrug.jsx
+++ src/pages/EditDrug.jsx
@@ -147,7 +147,7 @@ export default function EditDrug() {
                                                 id="quantity"
                                                 value={editData.quantity}
                                                 handleSaveChanges={handleSaveChanges}
-                                                placeholder="QUANTITY"
+                                                placeholder="Quantity"
                                             />
                                         </Fieldwrapper>
                                         <Fieldwrapper>
@@ -160,7 +160,7 @@ export default function EditDrug() {
                                                     id="threshold"
                                                     value={editData.threshold}
                                                     handleInputChange={handleInputChange}
-                                                    placeholder="MIN AMOUNT"
+                                                    placeholder="Min Amount"
                                                 />
                                             </div>
                                         </Fieldwrapper>
@@ -240,7 +240,6 @@ export const Fieldwrapper = styled.div`
         margin-bottom: 0.5rem;
     }
     input {
-        text-transform: uppercase;
         width: 100%;
         padding: 0.375rem 0.75rem;
         border-radius: var(--border-radius);
